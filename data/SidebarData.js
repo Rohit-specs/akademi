42ea7172fdf9076a524f424b2cd4510908372cc5
Rohit kumar <rohit5592006@gmail.com>
2026-06-22 23:37:14 +0530

    json data

---
+++ data/SidebarData.js
@@ -0,0 +1,58 @@
+import {
+  House,
+  Mortarboard,
+  PersonWorkspace,
+  Calendar3,
+  CurrencyDollar,
+  CupHot,
+  Person,
+  Chat,
+  Activity
+} from "react-bootstrap-icons";
+export const menuItems = [
+  {
+    title: "Dashboard",
+    icon: House,
+    path: "/dashboard",
+  },
+  {
+    title: "Students",
+    icon: Mortarboard,
+    path: "/students",
+  },
+  {
+    title: "Teachers",
+    icon: PersonWorkspace,
+    path: "/teachers",
+  },
+  {
+    title: "Events",
+    icon: Calendar3,
+    path: "/events",
+  },
+  {
+    title: "Finance",
+    icon: CurrencyDollar,
+    path: "/finance",
+  },
+  {
+    title: "Food",
+    icon: CupHot,
+    path: "/food",
+  },
+  {
+    title: "User",
+    icon: Person,
+    path: "/user",
+  },
+  {
+    title: "Chat",
+    icon: Chat,
+    path: "/chat",
+  },
+  {
+    title: "Latest Activity",
+    icon: Activity,
+    path: "/activity",
+  },
+];
---
+++ data/Theme.js
@@ -0,0 +1,15 @@
+export const COLORS = {
+  orange: "#FF7E5F",
+  yellow: "#FCC43E",
+  purple: "#4D44B5",
+  red: "#FF4550",
+  green: "#4CBC9A",
+  text: "#303972",
+};
+
+// $purple: #4D44B5;
+// $orange: #FB7D5B;
+// $yellow: #FCC43E;
+// $text: #303972;
+// $green: #4CBC9A;
+// $red: #FF4550;
