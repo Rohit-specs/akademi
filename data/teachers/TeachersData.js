378704fdace5c41c7c9aaa6176c11723d8af05fe
rohit-specs <rohit5592006@gmail.com>
2026-06-24 16:35:29 +0530

    updated

---
+++ data/teachers/TeachersData.js
@@ -0,0 +1,210 @@
+export const Teachers = [
+    {
+        "id": 1,
+        "name": "Christina Gosker",
+        "subject": "Political Science",
+        "profilePicture": "https://robohash.org/illovoluptatemipsum.png?size=50x50&set=set1",
+        "phone": "555-591-5685",
+        "email": "cgosker0@istockphoto.com"
+    },
+    {
+        "id": 2,
+        "name": "Brynne Derham",
+        "subject": "Physics",
+        "profilePicture": "https://robohash.org/occaecatiadsimilique.png?size=50x50&set=set1",
+        "phone": "879-276-1268",
+        "email": "bderham1@alexa.com"
+    },
+    {
+        "id": 3,
+        "name": "Lianne McCoy",
+        "subject": "Mathematics",
+        "profilePicture": "https://robohash.org/etvoluptasvoluptatem.png?size=50x50&set=set1",
+        "phone": "504-746-0467",
+        "email": "lmccoy2@tripod.com"
+    },
+    {
+        "id": 4,
+        "name": "Leonard Rosenau",
+        "subject": "English",
+        "profilePicture": "https://robohash.org/sitdoloremautem.png?size=50x50&set=set1",
+        "phone": "646-978-5665",
+        "email": "lrosenau3@wikia.com"
+    },
+    {
+        "id": 5,
+        "name": "Annette Jansen",
+        "subject": "Economics",
+        "profilePicture": "https://robohash.org/porrononfuga.png?size=50x50&set=set1",
+        "phone": "667-282-9543",
+        "email": "ajansen4@zimbio.com"
+    },
+    {
+        "id": 6,
+        "name": "Graham Sabie",
+        "subject": "History",
+        "profilePicture": "https://robohash.org/assumendaabcommodi.png?size=50x50&set=set1",
+        "phone": "222-804-5370",
+        "email": "gsabie5@ning.com"
+    },
+    {
+        "id": 7,
+        "name": "Stillmann Borsay",
+        "subject": "English",
+        "profilePicture": "https://robohash.org/etquiaitaque.png?size=50x50&set=set1",
+        "phone": "680-758-0025",
+        "email": "sborsay6@amazon.de"
+    },
+    {
+        "id": 8,
+        "name": "Liesa Heberden",
+        "subject": "Mathematics",
+        "profilePicture": "https://robohash.org/distinctiovoluptatemesse.png?size=50x50&set=set1",
+        "phone": "876-972-2134",
+        "email": "lheberden7@aboutads.info"
+    },
+    {
+        "id": 9,
+        "name": "Vina Warlock",
+        "subject": "Physics",
+        "profilePicture": "https://robohash.org/facilisperspiciatisofficia.png?size=50x50&set=set1",
+        "phone": "810-641-9535",
+        "email": "vwarlock8@constantcontact.com"
+    },
+    {
+        "id": 10,
+        "name": "Laurella Gregorio",
+        "subject": "Sociology",
+        "profilePicture": "https://robohash.org/etquasminima.png?size=50x50&set=set1",
+        "phone": "785-830-8051",
+        "email": "lgregorio9@addtoany.com"
+    },
+    {
+        "id": 11,
+        "name": "Nancie Ramiro",
+        "subject": "English",
+        "profilePicture": "https://robohash.org/temporibusvoluptasfugiat.png?size=50x50&set=set1",
+        "phone": "717-638-5017",
+        "email": "nramiroa@gnu.org"
+    },
+    {
+        "id": 12,
+        "name": "Donica Gerg",
+        "subject": "History",
+        "profilePicture": "https://robohash.org/quaeratoccaecatiodio.png?size=50x50&set=set1",
+        "phone": "840-470-4252",
+        "email": "dgergb@adobe.com"
+    },
+    {
+        "id": 13,
+        "name": "Kerrill Bidwell",
+        "subject": "Biology",
+        "profilePicture": "https://robohash.org/atconsequaturlabore.png?size=50x50&set=set1",
+        "phone": "933-979-4904",
+        "email": "kbidwellc@kickstarter.com"
+    },
+    {
+        "id": 14,
+        "name": "Mead Pexton",
+        "subject": "History",
+        "profilePicture": "https://robohash.org/quosautquibusdam.png?size=50x50&set=set1",
+        "phone": "948-743-4274",
+        "email": "mpextond@baidu.com"
+    },
+    {
+        "id": 15,
+        "name": "Avie Thams",
+        "subject": "Biology",
+        "profilePicture": "https://robohash.org/autlaborumpraesentium.png?size=50x50&set=set1",
+        "phone": "983-806-9592",
+        "email": "athamse@people.com.cn"
+    },
+    {
+        "id": 16,
+        "name": "Amabel Setford",
+        "subject": "Biology",
+        "profilePicture": "https://robohash.org/idpariaturet.png?size=50x50&set=set1",
+        "phone": "247-451-7689",
+        "email": "asetfordf@wisc.edu"
+    },
+    {
+        "id": 17,
+        "name": "Nerissa Maudett",
+        "subject": "Geography",
+        "profilePicture": "https://robohash.org/innemoearum.png?size=50x50&set=set1",
+        "phone": "626-385-2509",
+        "email": "nmaudettg@skyrock.com"
+    },
+    {
+        "id": 18,
+        "name": "Dunn Stringman",
+        "subject": "Mathematics",
+        "profilePicture": "https://robohash.org/voluptatibusvoluptatemquia.png?size=50x50&set=set1",
+        "phone": "584-755-0175",
+        "email": "dstringmanh@telegraph.co.uk"
+    },
+    {
+        "id": 19,
+        "name": "Shaylah Stobbe",
+        "subject": "Biology",
+        "profilePicture": "https://robohash.org/ullamquidemcorrupti.png?size=50x50&set=set1",
+        "phone": "198-102-6515",
+        "email": "sstobbei@domainmarket.com"
+    },
+    {
+        "id": 20,
+        "name": "Vanya Folger",
+        "subject": "Mathematics",
+        "profilePicture": "https://robohash.org/perferendisdeseruntiure.png?size=50x50&set=set1",
+        "phone": "331-249-8393",
+        "email": "vfolgerj@epa.gov"
+    },
+    {
+        "id": 21,
+        "name": "Amity Tunnock",
+        "subject": "Sociology",
+        "profilePicture": "https://robohash.org/etesttemporibus.png?size=50x50&set=set1",
+        "phone": "728-985-0441",
+        "email": "atunnockk@intel.com"
+    },
+    {
+        "id": 22,
+        "name": "Tannie Rapelli",
+        "subject": "Chemistry",
+        "profilePicture": "https://robohash.org/quisquameiuscupiditate.png?size=50x50&set=set1",
+        "phone": "147-334-3842",
+        "email": "trapellil@who.int"
+    },
+    {
+        "id": 23,
+        "name": "Sigvard Lewer",
+        "subject": "Psychology",
+        "profilePicture": "https://robohash.org/facereaccusamusconsectetur.png?size=50x50&set=set1",
+        "phone": "598-807-7340",
+        "email": "slewerm@epa.gov"
+    },
+    {
+        "id": 24,
+        "name": "Elga Costell",
+        "subject": "Economics",
+        "profilePicture": "https://robohash.org/oditautfugiat.png?size=50x50&set=set1",
+        "phone": "745-283-8993",
+        "email": "ecostelln@squarespace.com"
+    },
+    {
+        "id": 25,
+        "name": "Shoshana Cranfield",
+        "subject": "Physics",
+        "profilePicture": "https://robohash.org/sedetconsequatur.png?size=50x50&set=set1",
+        "phone": "496-993-7083",
+        "email": "scranfieldo@yandex.ru"
+    },
+    {
+        "id": 26,
+        "name": "Ezequiel Basezzi",
+        "subject": "Computer Science",
+        "profilePicture": "https://robohash.org/etaliquamsuscipit.png?size=50x50&set=set1",
+        "phone": "237-528-7535",
+        "email": "ebasezzip@blogspot.com"
+    }
+]
