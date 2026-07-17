5b77ae7ed93fcc559c989fa6f9756ca8495a40d4
Rohit <rohit5592006@gmail.com>
2026-07-17 22:31:47 +0530

    change project name and component

--- component/Sidebar.js
+++ component/Sidebar.js
@@ -1,5 +1,4 @@
 'use client'
-import { HeartFill } from "react-bootstrap-icons"
 import SidebarItem from "./SidebarItem"
 import { menuItems } from "/data/SidebarData"
 const Sidebar = () => {
@@ -7,7 +6,7 @@ const Sidebar = () => {
     <aside className='d-none d-sm-block sidebar bg-linear-3 vh-100 overflow-y-auto position-sticky top-0'>
         <div className='sidebar-logo icon-link p-lg-4 p-3'>
             <img src={"/logo/logo.svg"} alt="Akedemi Logo"/>
-            <h2 className="logo ms-2 text-light d-none d-lg-block">Akademi</h2>
+            <h2 className="logo ms-2 text-light d-none d-lg-block">Academi</h2>
         </div>
         <div className="sidebar-menu mb-lg-1">
             <ul className="d-flex flex-column ms-lg-4">
@@ -18,8 +17,8 @@ const Sidebar = () => {
             {/* p-lg-4 p-3 */}
         </div>
         <div className="sidebar-footer ms-3 mt-5">
-            <small className="text-light fs-small fw-bold d-none d-lg-inline">Akademi - School Admin <br/> Dashboard</small>
-            <small className="text-light d-none d-lg-block">Made with <span><HeartFill className="text-danger"/></span> by Peterdraw</small>
+            <small className="text-light fs-small fw-bold d-none d-lg-inline">Academi - School Admin <br/> Dashboard</small>
+            <small className="text-light d-none d-lg-block">© 2026 Indixpert</small>
         </div>
     </aside>
   )
